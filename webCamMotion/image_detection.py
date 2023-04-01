3d45f5de5b79c22e86dc291e53991d42075f68dd
Aleex-C <alexxcroitor9@gmail.com>
2023-04-01 22:57:34 +0300

    updated comments

--- webCamMotion/image_detection.py
+++ webCamMotion/image_detection.py
@@ -4,15 +4,20 @@ face_cascade=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 img=cv2.imread("image.jpg")
 
+#creating a gray copy of the image for better detection
 img_gray=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+#setting the rate of scaling (5%) for the picture (building the image pyramid -> regardless of the face's size, it will get recognized by the algorithm, provided
+# the scaleFactor is a decent one
 faces=face_cascade.detectMultiScale(img_gray, scaleFactor=1.05, minNeighbors=5)
 print(type(faces))
 print(faces)
 
 for x, y, w, h in faces:
+    # iterating over the faces list with (x,y) being the left-top corner of the rectangle and w,h = weight, height
     img=cv2.rectangle(img, (x,y), (x+w, y+h), (0, 0, 255), 3)
 
+#resizing my quite large picture (update: the cropped one is better, optional resizing)
 resized=cv2.resize(img, (int(img.shape[1]/3), int(img.shape[0]/3)))
 
 cv2.imshow("Image with Face Recognition!", resized)
--- webCamMotion/video_capture.py
+++ webCamMotion/video_capture.py
@@ -17,31 +17,37 @@ while True:
         first_frame = gray
         continue
 
+    #create the differences between the background frame and the new captured ones
     delta_frame=cv2.absdiff(first_frame, gray)
-
+    #if delta is higher than 40 than the "spots" are whitened while the rest are "blackened"
     tsh_frame = cv2.threshold(delta_frame, 40, 255, cv2.THRESH_BINARY)[1]
     #smooth the white spots
     tsh_frame=cv2.dilate(tsh_frame, None, iterations=2)
+    #look for "white spots"
     (cnts, _) = cv2.findContours(tsh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in cnts:
         if cv2.contourArea(contour) < 10000:
             continue
+        #if the area of those white spots is "large enough" than there's definitely movement there so proceed to
+        #change status (it detected movement)
         status = 1
+        #plot rectangle)
         (x, y, w, h) = cv2.boundingRect(contour)
         cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 3)
     status_list.append(status)
+    #memory management (keeping only the last two start,end values)
     status_list=status_list[-2:]
     if status_list[-1]==1 and status_list[-2]==0:
         times.append(datetime.now())
     if status_list[-1]==0 and status_list[-2]==1:
         times.append(datetime.now())
+    
     cv2.imshow("Gray...", gray)
     cv2.imshow("Delta Frame", delta_frame)
     cv2.imshow("ThreshHold", tsh_frame)
     cv2.imshow("CAPTURING...", frame)
-
-
+    
     key=cv2.waitKey(1)
     if key==ord('q'):
         if status==1:
